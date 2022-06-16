2677fa4f2c1cc57e4b03550618c80fd610b84df9
GPC <anthonytguilherme@hotmail.com>
2022-06-16 03:22:54 -0300

    optmize line bar

--- components/Slides/index.js
+++ components/Slides/index.js
@@ -1,41 +1,106 @@
+const states = [true, false, false]
+
+function showItems(statesItems){
+    const item = Item`
+        .item{ 
+                list-style: none;
+                height: calc(var(--line-height) * 3);
+                width: calc(var(--line-height) * 3);
+                background-color: var(--happy-color);
+                border-radius: 50%;
+                transition: transform 100ms linear;
+                cursor: pointer;
+            }
+        `
+    const itemActive = Item`
+            .item.active{
+                transform: scale(1.7);
+            }
+
+            ${'active'}
+        `
+
+    const items = statesItems.map(state => {
+        if(state){
+            return itemActive
+        }
+        return item
+    })
+
+    return items.join('')
+}
+
+function createStyle(css){
+    const head = document.querySelector('head')
+    const style = `
+    <style>
+        ${css}
+    </style
+    `
+    head.insertAdjacentHTML('beforeend', style)
+
+}
+
+const Action = (css) => {
+    createStyle(css)
+    return (
+        `<li class="action"></li>`
+    )
+}
+
+function handleClick(event){
+    const { target } = event
+    const allItems = document.querySelectorAll('.item')
+
+    allItems.forEach(item => item.classList.remove('active'))
+    target.classList.add('active'); 
+}
 
 
+const Item = (css, className) => {  
+    createStyle(css)
 
+    return ( 
+        `<li class="item ${className}"
+        onclick="handleClick(event)"></li>`
+    )
+}
 
-
-
-const Action = (css, content = '')=>(
-    `<li style="${css}">${content}</li>`
-)
-
-
-const Item =(css, content = '')=>( 
-    `<li style="${css}">${content}</li>`
-)
-
-const Slide =(css, content)=>(
+const Slide = (css, content) => (
     `<ul style="${css}">${content}</ul>`
 )
 
 const action = Action`
-    list-style: none;
-    background-color: var(--happy-color2);
-    height: calc(var(--line-height) * 4);
-    width: calc(var(--line-height) * 4);
-    border-radius: 50%;
-    position: absolute;
-    left: 4.5px;
+    .action{
+        list-style: none;
+        background-color: var(--happy-color2);
+        height: calc(var(--line-height) * 4);
+        width: calc(var(--line-height) * 4);
+        border-radius: 50%;
+        position: absolute;
+        left: -5px;
+    }
 
+    .action.second{
+        transform: translateX(224px);
+    }
+
+    .action.thrid{
+        transform: translateX(447px);
+    }
+    
 `
 
 const item = Item`
-    list-style: none;
-    height: calc(var(--line-height) * 3);
-    width: calc(var(--line-height) * 3);
-    background-color: var(--happy-color);
-    border-radius: 50%;
-
+    .item{ 
+        list-style: none;
+        height: calc(var(--line-height) * 3);
+        width: calc(var(--line-height) * 3);
+        background-color: var(--happy-color);
+        border-radius: 50%;
+}
 `
+
 
 const slide = Slide`
     display: flex;
@@ -46,6 +111,6 @@ const slide = Slide`
     height: 10px;
     background-color: var(--happy-color);
     margin-top: auto;
-    ${item + item + item + action}
-
+    ${showItems(states) + action}
 `
+
